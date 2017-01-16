ed02bf333d5b0dbab56d726ddd2e6770b724300a
Dorian Delorme <dorian.delorme@hetic.net>
2017-01-16 14:57:18 +0100

    Change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-1000.times do |i|
-	Article.create({content: "Article numéro #{i}"})
-	puts "Creating article #{i}"
+5.times do |i|
+
+	puts "Creating Chapter #{i}"
+	chapter = Chapter.create({title: "Chapitre numéro #{i}",
+							  published_at: Time.now})
+
+	10.times do |j|
+
+		puts "	Creating Articles #{j} for chapter #{i}"
+		chapter.articles.create({content: "Contenu de l'article #{j} pour le chapitre #{i}"})
+
+	end
+
 end
 
-10.times do |i|
-	Chapter.create({title: "Chapitre #{i}"})
-	puts "Creating chapter #{i}"
-end
+#1000.times do |i|
+#	Article.create({content: "Article numéro #{i}"})
+#	puts "Creating article #{i}"
+#end
+#
+#10.times do |i|
+#	Chapter.create({title: "Chapitre #{i}"})
+#	puts "Creating chapter #{i}"
+#end
