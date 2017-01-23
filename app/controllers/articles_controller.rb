d92ddfa0915657e2b43f28f5b4ecd849e2c81198
Dorian Delorme <dorian.delorme@hetic.net>
2017-01-23 15:17:06 +0100

    Article publish status

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
 
-	def index
-		@articles = Article.all
-	end
+    before_action :fetch_article
+	#def index
+	#	@articles = Article.all
+	#end
 
     def show
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
+
+    private
+        def fetch_article
+            @article = Article.published.find(params[:id])
+        end
 end
